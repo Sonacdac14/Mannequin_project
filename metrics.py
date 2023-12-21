62a81cf36257f45205eeaff92989c80fca04089f
Sonali Koli <86095292+Sonacdac14@users.noreply.github.com>
2023-12-21 11:57:31 +0530

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -1,1 +1,21 @@
+import numpy as np
+import tensorflow as tf
+from keras import backend as K
 
+smooth = 1e-15
+def dice_coef(y_true, y_pred):
+    y_true = tf.keras.layers.Flatten()(y_true)
+    y_pred = tf.keras.layers.Flatten()(y_pred)
+    intersection = tf.reduce_sum(y_true * y_pred)
+    return (2. * intersection + smooth) / (tf.reduce_sum(y_true) + tf.reduce_sum(y_pred) + smooth)
+
+def dice_loss(y_true, y_pred):
+    return 1.0 - dice_coef(y_true, y_pred)
+
+def pixel_accuracy(y_true, y_pred):
+    y_true = tf.keras.layers.Flatten()(y_true)
+    y_pred = tf.keras.layers.Flatten()(y_pred)
+    y_pred_binary = K.round(y_pred)
+    correct_pixels = K.equal(y_true, y_pred_binary)
+    accuracy = K.mean(correct_pixels)
+    return accuracy
